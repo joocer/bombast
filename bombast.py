249fa97456314490f68174207f6a693e77e76c35
Justin Joyce <justin.joyce@joocer.com>
2020-11-15 23:42:16 +0000

    reworking script

--- bombast.py
+++ bombast.py
@@ -7,6 +7,8 @@ version available on pypi.
 import pkg_resources
 import requests
 from packaging import version
+from pkg_resources import parse_version
+import json
 
 
 class colors:
@@ -22,11 +24,28 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+COMPARATORS = {
+    '<':  lambda x, y: x < y,
+    '<=': lambda x, y: x <= y,
+    '>':  lambda x, y: x > y,
+    '>=': lambda x, y: x >= y,
+    '=':  lambda x, y: x == y
+}
+
+def get_known_vulns():
+    """
+    Look up known vulns from PyUp.io
+    """
+    #try:
+    #url = "https://raw.githubusercontent.com/pyupio/safety-db/master/data/insecure_full.json"
+    #resp = requests.get(url)
+    #data = resp.json()
+    data = json.load(open('insecure_full.json', 'r'))
+    return data
+    #except:
+    #    return 'unknown'
 
 def get_latest_version(package_name):
-    """
-    Look up the current release from PyPi.
-    """
     try:
         url = "https://pypi.org/pypi/{}/json".format(package_name)
         resp = requests.get(url)
@@ -35,15 +54,71 @@ def get_latest_version(package_name):
     except:
         return 'unknown'
 
-def main():
-    for package in pkg_resources.working_set:
-        installed_version = package.version
-        latest_version = get_latest_version(package.project_name)
-        package_name = package.project_name
+def compare_versions(version_a, version_b):
+    # default to equals
+    operator = COMPARATORS['=']
 
-        if installed_version == latest_version:
-            print(colors.GREEN + "✓ PASS: {} ({})".format(package_name, installed_version) + colors.END)
-        else:
-            print(colors.RED   + "✗ FAIL: {} ({}), latest: {}".format(package_name, installed_version, latest_version) + colors.END)
+    # find if it's a different operator
+    find_operator = [c for c in COMPARATORS if version_a.startswith(c)]
+    if len(find_operator):
+        s = find_operator[0]
+        operator = COMPARATORS[s]
+        version_a = version_a.lstrip(s)
+
+    return operator(parse_version(version_b), parse_version(version_a))
+
+
+def get_package_summary(package=None,
+                installed_version=None,
+                vuln_details={}):
+
+    result = { 
+        "package": package,
+        "installed_version": installed_version,
+        "state": "OKAY"
+    }
+    result['latest_version'] = get_latest_version(package_name=package)
+    if result['latest_version'] != result['installed_version']:
+        result['state'] = "STALE"
+
+    if vuln_details:
+        for i in vuln_details:
+
+                for version_pairs in i['specs']:
+                    versions = version_pairs.split(',')
+                    if len(versions) == 1:
+                        versions = ['>0'] + versions
+
+                    if compare_versions(versions[0], installed_version) and compare_versions(versions[1], installed_version):
+                        result['state'] = "VULNERABLE"
+                        result['cve'] = i.get('cve')
+                        result['reference'] = i.get('id')
+
+    return result
+
+
+
+
+def main():
+
+    results = []
+
+    known_vulns = get_known_vulns()
+    for package in pkg_resources.working_set:
+
+        #if package.project_name != 'lxml': continue
+        package_result = get_package_summary(package=package.project_name,
+                    installed_version=package.version,
+                    vuln_details=known_vulns.get(package.project_name))
+
+        print(package_result)
+
+       # if installed_version == latest_version:
+       #     print(colors.GREEN + "✓ PASS: {} ({})".format(package_name, installed_version) + colors.END)
+       # else:
+       #     print(colors.YELLOW   + "✗ STALE: {} ({}), latest: {}".format(package_name, installed_version, latest_version) + colors.END)
+
+#    print(results)
+
 
 main()
